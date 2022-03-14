eadb46dd5bc923b04051061d3af21080e99bcae5
buzinaro1203 <guilhermebuzinaro212@gmail.com>
2022-03-14 15:04:12 -0300

    projeto nao finalizado

---
+++ common/js/setup.js
@@ -0,0 +1,25 @@
+$(window).scroll(function () {
+	var scroll = $(window).scrollTop();
+	if (scroll > 1) {
+		$('.menu').addClass('menu-auxiliar');
+	} else {
+		$('.menu').removeClass('menu-auxiliar');
+	}
+});
+$('.owl-carousel').owlCarousel({
+	loop: true,
+	margin: 20,
+	nav: true,
+	center: true,
+	responsive: {
+		0: {
+			items: 2
+		},
+		600: {
+			items: 3
+		},
+		1000: {
+			items: 7
+		}
+	}
+})
