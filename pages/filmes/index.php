d660d22b045a6595595894c1aaf3452aa3e8e57f
buzinaro1203 <guilhermebuzinaro212@gmail.com>
2022-03-25 12:24:02 -0300

    :fire: feat: include php in all pages

---
+++ pages/filmes/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style/filmes.css">
+	<link rel="stylesheet" href="../../common/css/style-mobile.css">
+	<title>NETFLIX</title>
+	<!--owl css-->
+	<link rel="stylesheet" href="style/owl/owl.carousel.min.css">
+	<link rel="stylesheet" href="style/owl/owl.theme.default.min.css">
+
+</head>
+
+<body>
+
+
+	<main>
+		<div class="background">
+			<div class="content">
+				<!--HEADER-->
+				<div class="navbar">
+					<a href="#"><img class="spotflixlog" src="../../img/spotflixlog.png" alt="netflix-font" border="0"></a>
+					<ul role="list" class="nav-list">
+						<li class="nav-area"><a class="nav-text glitch" rel="shrink" href="../.."><span
+							class="span-glitch">Inicio</span><span class="span-glitch">Inicio</span><span
+							class="span-glitch">Inicio</span>Inicio</a></li>
+						<li class="nav-area"><a class="nav-text glitch" rel="shrink" href="../series/"><span
+									class="span-glitch">Séries</span><span class="span-glitch">Séries</span><span
+									class="span-glitch">Séries</span>Séries</a></li>
+						<li class="nav-area"><span class="nav-text active" rel="shrink">Filmes</span></li>
+						<li class="nav-area"><a class="nav-text glitch" rel="shrink" href="../animes/"><span
+									class="span-glitch">Animes</span><span class="span-glitch">Animes</span><span
+									class="span-glitch">Animes</span>Animes</a></li>
+					</ul>
+				</div>
+				<!--HEADER-END-->
+				<div class="background-wrapper">
+					<div class="background-conteiner">
+						<img class="tittle-logo" src="/img/logospider.png" alt="titulosherlock">
+						<h3 class="description">Escute agora as principais musicas da trilha sonora de <span
+								class="XDD">Homem-aranha no aranhaverso</span></h3>
+					</div>
+					<div class="button-layer">
+						<button role="button" class="button1">
+							<i class="fas fa-play"></i>
+							Escutar
+						</button>
+						<button role="button" class="button2">
+							<i class="fas fa-info-circle"></i>
+							Mais informações
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="list-layer">
+		<ul class="list">
+			<li class="list-wrapper"><img class="movie-img" src="img/minicapa10.jpg" alt="movie-poster">
+			</li>
+			<li class="list-wrapper"><img class="movie-img" src="img/minicapa2.jpg" alt="movie-poster">
+			</li>
+			<li class="list-wrapper"><img class="movie-img" src="img/minicapa5.jpg" alt="movie-poster">
+			</li>
+			<li class="list-wrapper"><img class="movie-img" src="img/minicapa4.jpg" alt="movie-poster">
+			</li>
+			<li class="list-wrapper"><img class="movie-img" src="img/minicapa7.jpg" alt="movie-poster">
+			</li>
+			<li class="list-wrapper"><img class="movie-img" src="img/minicapa3.jpg" alt="movie-poster">
+			</li>
+			<li class="list-wrapper"><img class="movie-img" src="img/minicapa8.jpg" alt="movie-poster">
+			</li>
+			<li class="list-wrapper"><img class="movie-img" src="img/minicapa6.jpg" alt="movie-poster">
+			</li>
+			<li class="list-wrapper"><img class="movie-img" src="img/minicapa9.jpg" alt="movie-poster">
+			</li>
+			<li class="list-wrapper"><img class="movie-img" src="img/minicapa1.jpg" alt="movie-poster">
+			</li>
+		</ul>
+	</div>
+	<!--FOOTER-->
+	<?php include '../../common/php/footer.php' ?>
+	<!--FOOTEREND-->
+	</main>
+	<script src="../../common/js/owl/jquery.min.js"></script>
+	<script src="../../common/js/owl/owl.carousel.min.js"></script>
+	<script src="../../common/js/setup.js"></script>
+	<script src="https://kit.fontawesome.com/b0769da048.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
